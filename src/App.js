4ed2b18447a3c9528d6caede8c55d11a310460a3
kvsrajawat <kvs.rajawat.bh@gmail.com>
2020-03-11 20:14:02 +0530

    added index,login,navbar components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import PriceList from "./Components/PriceList/PriceList"
 import Index from "./Components/AppIndex/Index"
+import Login from './Components/Login/Login';
 
 class App extends Component {
   render()
@@ -13,6 +14,7 @@ class App extends Component {
           <Switch>
             <Route path="/" exact component={Index}></Route>
             <Route path="/beer" exact component={PriceList}></Route>
+            <Route path="/login" exact component={Login}></Route>
             </Switch>
         </Router>
         
